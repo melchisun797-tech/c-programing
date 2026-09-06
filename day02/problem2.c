87e3ddfe0508aed25c33b48f6ead1940215c7cbe
melchisun797-tech <melchisun797@gmail.com>
2026-09-06 17:27:24 +0530

    Update problem2.c

--- day02/problem2.c
+++ day02/problem2.c
@@ -1,11 +1,22 @@
-Problem 2
-
+problem 2
 Ask the user for:
-
 length
 width
-
 Calculate and print:
-
 Area
 Perimeter
+
+
+
+#include <stdio.h>
+int main()
+{
+    float l;
+    float w;
+    printf("enter the length and widgth of a rectangle");
+    scanf("%f",&l);
+    scanf("%f",&w);
+    printf("the perimeter of rectangle is %1.f\n",l*w);
+    printf("the area of rectangle is %1.f\n",(l+w)*2);
+    return 0;
+}
