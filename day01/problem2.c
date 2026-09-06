a11e3545cb9da0e3636b2532b26d22f65d621610
melchisun797-tech <melchisun797@gmail.com>
2026-09-06 16:48:35 +0530

    Update problem2.c

--- day01/problem2.c
+++ day01/problem2.c
@@ -1,1 +1,16 @@
+//Problem 2
+//Write a program that asks the user for two integers and prints both numbers.
 
+
+#include <stdio.h>
+int main()
+{
+    int fno;
+    int sno;
+    printf("give me 2 integer nos=");
+    scanf("%d",&fno);
+    scanf("%d",&sno);
+    printf("frist no is %d\n",fno);
+    printf("second no is %d\n",sno);
+    return 0;
+}
