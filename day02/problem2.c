7274c72c8fe3302f670ec96a43ca3d346c123f7b
melchisun797-tech <melchisun797@gmail.com>
2026-09-06 18:26:38 +0530

    Update problem2.c

--- day02/problem2.c
+++ day02/problem2.c
@@ -16,7 +16,7 @@ int main()
     printf("enter the length and widgth of a rectangle");
     scanf("%f",&l);
     scanf("%f",&w);
-    printf("the perimeter of rectangle is %1.f\n",l*w);
-    printf("the area of rectangle is %1.f\n",(l+w)*2);
+    printf("the area of rectangle is %1.f\n",l*w);
+    printf("the perimeter of rectangle is %1.f\n",(l+w)*2);
     return 0;
 }
