c8c1d22525880c3adbf42a292365d9f976cd6784
melchisun797-tech <melchisun797@gmail.com>
2026-09-06 17:54:35 +0530

    Create problem4.c

---
+++ day02/problem4.c
@@ -0,0 +1,24 @@
+//problem 4
+//Ask the user for a three-digit integer:
+//Your program should output:
+//Hundreds digit = 5
+//Tens digit = 8
+//Ones digit = 3
+
+
+#include <stdio.h>
+int main() 
+{
+    int n;
+    printf("enter a 3 digit no");
+    scanf("%d",&n);
+    int f1=n%10;
+    n=n/10;
+    int f2=n%10;
+    n=n/10;
+    int f3=n%10;
+    printf("the hundreds digits is %d\n",f1);
+    printf("the tens digits is %d\n",f2);
+    printf("the onces digits is %d\n",f3);
+    return 0;
+}
