220c431b619c0c54e1a210a0e5ec70ae181e1cf4
melchisun797-tech <melchisun797@gmail.com>
2026-09-06 16:46:39 +0530

    Update problem1.c

--- day01/problem1.c
+++ day01/problem1.c
@@ -1,1 +1,19 @@
+//Problem 1
+//Write a C program that stores your:
+//age
+//height
+//first initial
+//and prints all three.
 
+
+#include <stdio.h>
+int main()
+{
+    int age=18;
+    float height=168.6;
+    char intial='m';
+    printf("my age is %d\n",age);
+    printf("my height is %.1f\n",height);
+    printf("my intial is %c\n",intial);
+    return 0;
+}
