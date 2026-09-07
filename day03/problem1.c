8a664fa48a0d1c71403935b23503e5705317290c
melchisun797-tech <melchisun797@gmail.com>
2026-09-07 18:55:23 +0530

    Update problem1.c

--- day03/problem1.c
+++ day03/problem1.c
@@ -1,1 +1,23 @@
+//Write a C program that:
+//Takes an integer from the user.
+//Checks whether the number is positive or negative.
+//Prints the appropriate result.
 
+
+include <stdio.h>
+int main()
+{
+    int n;
+    printf("give a integer");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("the no is positive");
+    }
+    else if (n<0)
+    {
+        printf("the no is negative");
+    }
+    return 0;
+}
+#
