f1473d2efa658132a3d176cfa8d798d3dfb24ec4
melchisun797-tech <melchisun797@gmail.com>
2026-09-08 18:48:26 +0530

    Create problem2.c

---
+++ day04/problem2.c
@@ -0,0 +1,19 @@
+//Write a program that prints:
+//5
+//4
+//3
+//2
+//1
+
+#include <stdio.h>
+int main() 
+{
+    int n;
+    n=5;
+    while(n>0)
+        {
+            printf("%d\n",n);
+            n--;
+        }
+    return 0;
+}
