e69cc4ba3867c2d3a00f2b0b33632b5d8c4570f6
melchisun797-tech <melchisun797@gmail.com>
2026-09-08 18:42:28 +0530

    Create problem1.c

---
+++ day04/problem1.c
@@ -0,0 +1,21 @@
+//Write a C program that prints the numbers:
+//1
+//2
+//3
+//4
+//5
+
+
+#include <stdio.h>
+int main() 
+{
+    int n;
+    n=1;
+    for(int i=0;i<5;i++)
+        {
+            printf("%d\n",n);
+            n++;
+        }
+   
+    return 0;
+}
