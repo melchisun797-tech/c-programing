7c6920c714944286ef6e4fc4f0c2177633f391aa
melchisun797-tech <melchisun797@gmail.com>
2026-09-10 19:22:38 +0530

    Update problem4.c

--- day06/problem4.c
+++ day06/problem4.c
@@ -9,16 +9,14 @@
 #include <stdio.h>
 int main()
 {
-    int prod;
+    char c='*';
     for(int i=1;i<=5;i++)
         {
-          for(int j=1;j<=10;j++)
+          for(int j=1;j<=i;j++)
               {
-                  prod=i*j;
-                  printf("%d x %d = %d\n",i,j,prod);  
+                  printf("%c",c);  
               }
             printf("\n");
-            
         }
      return 0;
 }
