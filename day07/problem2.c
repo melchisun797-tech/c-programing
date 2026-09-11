666f4f8db6436d5abfa42bd283d412ef1e697ffb
melchisun797-tech <melchisun797@gmail.com>
2026-09-11 18:34:25 +0530

    Create problem2.c

---
+++ day07/problem2.c
@@ -0,0 +1,16 @@
+//Write a function called add() that takes two integers and prints their sum.
+//Expected output for inputs 10 and 20:
+//Sum = 30
+
+
+#include <stdio.h>
+int add(int a,int b)
+{
+    printf("the sum=%d",a+b);
+}
+int main() 
+{
+    add(10,20);
+    
+    return 0;
+}
