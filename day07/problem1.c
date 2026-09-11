f4649ff5f5ee188cd9e12fdc217dc75fac074176
melchisun797-tech <melchisun797@gmail.com>
2026-09-11 18:23:24 +0530

    Create problem1.c

---
+++ day07/problem1.c
@@ -0,0 +1,17 @@
+//Problem 1
+//Create a function that prints:
+//Hello World
+
+
+
+#include <stdio.h>
+void world()
+{
+    printf("hello world");
+}
+int main() 
+{
+    world();
+    
+    return 0;
+}
