04e30ca839453a31dafbc1231cc875796955f473
melchisun797-tech <melchisun797@gmail.com>
2026-09-12 19:48:21 +0530

    Update problem3.c

--- day08/problem3.c
+++ day08/problem3.c
@@ -1,1 +1,21 @@
-Ask the user for 5 integers, store them in an array, and find their sum using a loop.
+//Ask the user for 5 integers, store them in an array, and find their sum using a loop.
+
+
+#include <stdio.h>
+int main()
+{
+    int arr[5];
+    int sum=0;
+    printf("enter 5 no");
+    for(int i=0;i<=4;i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+    for(int j=0;j<=4;j++)
+        {
+            sum=sum+arr[j];
+        }
+    printf("%d",sum);
+    return 0;
+}
+1
