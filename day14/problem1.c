554421a19db046dc4941e0ec9080ccff87bc3c98
melchisun797-tech <melchisun797@gmail.com>
2026-09-18 17:52:20 +0530

    Update problem1.c

--- day14/problem1.c
+++ day14/problem1.c
@@ -1,1 +1,20 @@
+//Write a program that:
+//Takes a string from the user.
+//Creates a pointer to the string.
+//Uses *(p+i) to print every character.
+//Don't use name[i] for printing.
 
+
+#include <stdio.h>
+int main() 
+{
+    char c[6];
+    printf("enter a string");
+    scanf("%s",c);
+    char *p=c;
+    for(int i=0;i<=5;i++)
+        {
+            printf("%c",*(p+i));
+        }
+    return 0;
+}
