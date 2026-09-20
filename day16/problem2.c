2b320d181c7230c70f0d2bb0c396a8abc9bb69cd
melchisun797-tech <melchisun797@gmail.com>
2026-09-20 19:08:13 +0530

    Update problem2.c

--- day16/problem2.c
+++ day16/problem2.c
@@ -1,1 +1,32 @@
+//Write a program that:
+//Asks the user for n.
+//Uses calloc() to allocate n integers.
+//Takes n integers as input.
+//Finds the sum.
+//Prints the sum.
+//Uses free().
 
+
+#include <stdio.h>
+#include <stdlib.h> 
+int main()
+{
+    int *p;
+    int n;
+    int sum=0;
+    printf("enter the no of elements");
+    scanf("%d",&n);
+    p=calloc(n,sizeof(int));
+    printf("enter the elements");
+    for(int i=0;i<n;i++)
+        {
+                scanf("%d",&p[i]);
+        }
+    for(int i=0;i<n;i++)
+        {
+               sum=p[i]+sum;
+        }
+    printf("the sum of elements  %d",sum);
+    free(p);
+    return 0;
+}
