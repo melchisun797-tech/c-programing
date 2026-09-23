73a88561296578ca84bce0b53e2a55ab94901dc8
melchisun797-tech <melchisun797@gmail.com>
2026-09-23 18:53:48 +0530

    Create problem1.c

---
+++ day19/problem1.c
@@ -0,0 +1,24 @@
+//Create a union variable s
+//Store an age of 20
+//Print the age
+//Store a CGPA of 8.5
+//Print the CGPA
+
+
+#include <stdio.h>
+union student
+{
+    float cgpa;
+    int age;
+};
+int main()
+{
+    union student s;
+    printf("enter age\n");
+    scanf("%d",&s.age);
+    printf("age is= %d\n",s.age);
+    printf("enter cgpa\n");
+    scanf("%f",&s.cgpa);
+    printf("cgpa is= %d\n",s.cgpa);
+    return 0;
+}
