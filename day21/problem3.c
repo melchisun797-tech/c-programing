98d0b502b159988c21e5722eb60e1191f72406a2
melchisun797-tech <melchisun797@gmail.com>
2026-09-25 19:18:13 +0530

    Update problem3.c

--- day21/problem3.c
+++ day21/problem3.c
@@ -8,6 +8,6 @@ int main()
     int a,b;
     printf("tell me the nos");
     scanf("%d %d",&a,&b);
-    printf("the bigger nois %d",max(a,b));
+    printf("the bigger no is %d",max(a,b));
     return 0;
 }
