501b0b06e37106fd7a2d6e9a62079c2a5889bc56
melchisun797-tech <melchisun797@gmail.com>
2026-09-26 20:01:20 +0530

    Create day3.c

---
+++ day22/day3.c
@@ -0,0 +1,11 @@
+//Now write a program that prints the second argument.
+
+
+
+#include <stdio.h>
+int main( int argc,char *argv[])
+{
+    //program prints hello world
+    printf("%s",argv[2]);
+    return 0;
+}
