ce722c70d7bd97f15f934caa8ba95f8b50b8175d
melchisun797-tech <melchisun797@gmail.com>
2026-09-26 19:58:48 +0530

    Create problem2.c

---
+++ day22/problem2.c
@@ -0,0 +1,10 @@
+//Write a program that prints only the first argument given by the user.
+
+
+#include <stdio.h>
+int main( int argc,char *argv[])
+{
+    //program prints hello world
+    printf("%s",argv[1]);
+    return 0;
+}
