5490ef15c21b894b50d1b56b863758ae94cbce07
melchisun797-tech <melchisun797@gmail.com>
2026-09-26 19:57:59 +0530

    Create problem1.c

---
+++ day22/problem1.c
@@ -0,0 +1,14 @@
+Write a program that prints all command-line arguments.
+
+  
+#include <stdio.h>
+int main( int argc,char *argv[])
+{
+    //program prints hello world
+    printf("hello world");
+    for(int i=0;i<argc;i++)
+        {
+            printf("%s",argv[i]);
+        }
+    return 0;
+}
